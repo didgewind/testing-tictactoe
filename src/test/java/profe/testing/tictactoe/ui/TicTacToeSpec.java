19f434fd34a391fe50b0afd7c415b140dc94487e
made <kike@mazarredos.com>
2019-02-20 11:07:57 +0100

    Primer commit.
    Pom con junit 4.12, código primer test, play() vacío

---
+++ src/test/java/profe/testing/tictactoe/ui/TicTacToeSpec.java
@@ -0,0 +1,26 @@
+package profe.testing.tictactoe.ui;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class TicTacToeSpec {
+
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
+
+	private TicTacToe ticTacToe;
+
+	@Before
+	public final void before() {
+		ticTacToe = new TicTacToe();
+	}
+
+	@Test
+	public void whenXOutsideBoardThenRuntimeException() {
+		exception.expect(RuntimeException.class);
+		ticTacToe.play(5, 2);
+	}
+
+}
